444ffdeaae0e616fee43ba385494121320f0f143
gomitand <gomitand@gmail.com>
2024-03-19 22:38:10 -0500

    registro realizado
    realizado registro

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './src/views/home/home';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hola </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export type RootStackParamList = {
+  HomeScreen: undefined,
+  RegisterScreen: undefined,
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+        />
+        
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
